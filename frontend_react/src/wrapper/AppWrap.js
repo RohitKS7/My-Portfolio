9e8dd48b01ce5abb9f5f3efcedac97d2e8553df7
Rohit Kumar Suman <rohitkumarsuman2011india@gmail.com>
2023-02-01 20:11:21 +0530

    skills updated

--- frontend_react/src/wrapper/AppWrap.js
+++ frontend_react/src/wrapper/AppWrap.js
@@ -5,7 +5,13 @@ const AppWrap = (Component, idName, classNames) =>
   function HOC() {
     return (
       <div id={idName} className={`app__container ${classNames}`}>
-        <SocialMedia />
+        {idName === "home" ? (
+          <div style={{ display: "none" }}>
+            <SocialMedia />
+          </div>
+        ) : (
+          <SocialMedia />
+        )}
         <div className="app__wrapper app__flex">
           <Component />
 
